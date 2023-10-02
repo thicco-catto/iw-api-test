2fb77ac4bcd9e4f78e80744831f8a03bc29d4e8e
thicco-catto <cristobal.martin01@gmail.com>
2023-10-02 19:03:46 +0200

    Using env

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,17 +1,22 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 
-const uri = "mongodb+srv://cristobal:sanchoelmejor@practica.a1ieyro.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+const uri = process.env.MONGODB_URI;
+let clientPromise: Promise<MongoClient>;
+if (uri === undefined) {
+    console.log("[ERROR] Need to define MongoDB uri as an environment variable");
+} else {
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
 
-const clientPromise = client.connect();
+    clientPromise = client.connect();
+}
 
 export default async function GetDatabase() {
     return await clientPromise;
